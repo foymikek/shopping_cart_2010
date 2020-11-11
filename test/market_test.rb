37b1d45758ded687e907d9c3c372723249538812
Michael Foy <foymikek@gmail.com>
2020-11-11 09:28:39 -0700

    Add initializing Market class with vendors

--- test/market_test.rb
+++ test/market_test.rb
@@ -7,6 +7,8 @@ require "./lib/market"
 class MarketTest < Minitest::Test
   def test_it_exists_and_has_attributes
     market = Market.new("South Pearl Street Farmers Market")
+
+    assert_equal [], market.vendors
     assert_equal "South Pearl Street Farmers Market", market.name
   end
 end
