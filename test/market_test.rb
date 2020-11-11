a29d9b0dd622dc6dc6adfacac3bd6be607c9db11
Michael Foy <foymikek@gmail.com>
2020-11-11 09:26:58 -0700

    Add working test_it_exists_and_has_attributes

---
+++ test/market_test.rb
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/item"
+require "./lib/vendor"
+require "./lib/market"
+
+class MarketTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    market = Market.new("South Pearl Street Farmers Market")
+    assert_equal "South Pearl Street Farmers Market", market.name
+  end
+end
