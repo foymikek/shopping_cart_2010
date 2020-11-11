1de218db6dd909391afa1fe40dcb545daccd5047
Michael Foy <foymikek@gmail.com>
2020-11-11 09:32:05 -0700

    Add working test_market_can_add_vendors

--- test/market_test.rb
+++ test/market_test.rb
@@ -11,4 +11,27 @@ class MarketTest < Minitest::Test
     assert_equal [], market.vendors
     assert_equal "South Pearl Street Farmers Market", market.name
   end
+
+  def test_market_can_add_vendors
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor3 = Vendor.new("Palisade Peach Shack")
+
+    assert_equal [], market.vendors
+
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+    assert_equal [vendor1, vendor2, vendor3], market.vendors
+  end
 end
+
+# item1 = Item.new({name: 'Peach', price: "$0.75"})
+# #=> #<Item:0x007f9c56740d48...>
+# item2 = Item.new({name: 'Tomato', price: "$0.50"})
+# #=> #<Item:0x007f9c565c0ce8...>
+# item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+# #=> #<Item:0x007f9c562a5f18...>
+# item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+# #=> #<Item:0x007f9c56343038...>
