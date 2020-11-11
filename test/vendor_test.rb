6830e5c509c283d82948d4cf8ebbf59d4bad3b1b
Michael Foy <foymikek@gmail.com>
2020-11-11 09:15:02 -0700

    Add working test_it_exists_and_has_attributes

---
+++ test/vendor_test.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/item"
+require "./lib/vendor"
+
+class VendorTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_equal "Rocky Mountain Fresh", vendor.name
+    expected = {}
+    assert_equal expected, vendor.inventory
+  end
+end
